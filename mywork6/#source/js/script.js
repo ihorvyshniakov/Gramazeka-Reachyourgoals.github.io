3f297f8314c2db3a5da610b114cf27d66a88bbbd
Gramazeka-Reachyourgoals <gramazekavip@gmail.com>
2020-09-12 15:17:40 +0300

    1

--- mywork6/#source/js/script.js
+++ mywork6/#source/js/script.js
@@ -1,1 +1,6 @@
-console.log("Lolli-pop is here!");
+$(document).ready(function () {
+	$('nav').click(function (event) {
+		$('#burger, #img img, header').toggleClass('active');
+		$('body').toggleClass('lock');
+	});
+});
