f4de9231894d9f33805072368afea40c671c6252
Gramazeka-Reachyourgoals <gramazekavip@gmail.com>
2020-09-13 17:52:21 +0300

    Final before full research

--- mywork6/#source/js/script.js
+++ mywork6/#source/js/script.js
@@ -1,6 +1,23 @@
 $(document).ready(function () {
+	parent = document.querySelectorAll('.parent');
 	$('nav').click(function (event) {
 		$('#burger, #img img, header').toggleClass('active');
 		$('body').toggleClass('lock');
 	});
+	// $('.item_global:eq(0)').click(function (event) {
+	// 	$('h4:eq(0)').toggleClass('parent');
+	// 	$('.item:eq(0)').toggleClass('active');
+	// });
+	// if ($('.item_global:eq(0) h4:eq(0)').hasClass('parent')) {
+	// 	$('.item_global:eq(0) h4:eq(0)').removeClass('parent');
+	// } else {
+	// 	$('.item_global:eq(1)').click(function (event) {
+	// 		$('h4:eq(1)').toggleClass('parent');
+	// 		$('.item:eq(1)').toggleClass('active');
+	// 	});
+	// 	$('.item_global:eq(2)').click(function (event) {
+	// 		$('h4:eq(2)').toggleClass('parent');
+	// 		$('.item:eq(2)').toggleClass('active');
+	// 	});
+	// }
 });
