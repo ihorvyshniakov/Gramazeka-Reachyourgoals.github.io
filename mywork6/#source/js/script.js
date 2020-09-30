8dd461059c22c8aa7a82d0409e6936653a434ff1
Gramazeka-Reachyourgoals <gramazekavip@gmail.com>
2020-09-30 14:23:25 +0300

    Add accordion jQuery

--- mywork6/#source/js/script.js
+++ mywork6/#source/js/script.js
@@ -4,20 +4,42 @@ $(document).ready(function () {
 		$('#burger, #img img, header').toggleClass('active');
 		$('body').toggleClass('lock');
 	});
-	// $('.item_global:eq(0)').click(function (event) {
-	// 	$('h4:eq(0)').toggleClass('parent');
-	// 	$('.item:eq(0)').toggleClass('active');
-	// });
-	// if ($('.item_global:eq(0) h4:eq(0)').hasClass('parent')) {
-	// 	$('.item_global:eq(0) h4:eq(0)').removeClass('parent');
-	// } else {
-	// 	$('.item_global:eq(1)').click(function (event) {
-	// 		$('h4:eq(1)').toggleClass('parent');
-	// 		$('.item:eq(1)').toggleClass('active');
-	// 	});
-	// 	$('.item_global:eq(2)').click(function (event) {
-	// 		$('h4:eq(2)').toggleClass('parent');
-	// 		$('.item:eq(2)').toggleClass('active');
-	// 	});
-	// }
+
+	// hide all ul excet first
+	$('.item_global>ul').hide();
+
+	$('.item_global>h4').click(function () {
+
+		// find exact h4 on which user click & take next ul(list) for next actions
+		var findArticle = $(this).next('ul');
+
+		// find wrapper for our h4, it's need if on the page exist more than 1 accordion
+		var findWrapper = $(this).closest('#block2');
+
+		// add active status to h4
+		$(this).addClass('active');
+
+		// if user click on exact elem and it shown
+		if (findArticle.is(':visible')) {
+
+			// than hide it with animation slideUp(we can use 'fast'=10 & 'slow'=600 & default(empty)=400 - miliseconds)
+			findArticle.slideUp();
+
+			// toggle active status to h4
+			$(this).toggleClass('active');
+
+		} else {
+
+			// if elem is hide than hide all others .item-global>.item(lists) with anim slideUp 400ms
+			findWrapper.find('>.item_global>.item').slideUp();
+			// remove active status from all h4
+			findWrapper.find('>.item_global>h4').removeClass('active');
+
+			// and list of clicked h4 show with slideDown
+			findArticle.slideDown();
+			// add active status to clicked h4
+			findArticle.prev('h4').addClass('active');
+
+		};
+	});
 });
