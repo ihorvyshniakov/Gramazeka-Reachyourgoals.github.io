6ce172f12cda0b48174004108c3d708613a51b0a
Vyshniakov Ihor <gramazekavip@gmail.com>
2021-07-20 21:21:00 +0300

    - deploy first e-shop on react

---
+++ e-origami/precache-manifest.c4b698712c8c8e4f074175c28f57459c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b2d39ffbaa8fb01dc2e6ee26d935256",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a994c48868d60e40e432",
+    "url": "/static/css/main.fd967003.chunk.css"
+  },
+  {
+    "revision": "a56e8f6f4525ea115293",
+    "url": "/static/js/2.c74b237e.chunk.js"
+  },
+  {
+    "revision": "f7e623707d9b37a3821ec4c3548a4309",
+    "url": "/static/js/2.c74b237e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c36ca9af03bfaf0418e0",
+    "url": "/static/js/3.15c73d6a.chunk.js"
+  },
+  {
+    "revision": "a994c48868d60e40e432",
+    "url": "/static/js/main.de23dcdf.chunk.js"
+  },
+  {
+    "revision": "1e3a0fdf94e8f007b79b",
+    "url": "/static/js/runtime-main.a2c730b0.js"
+  },
+  {
+    "revision": "92b3c4b1d6a8acf9e38cbba2293a8dba",
+    "url": "/static/media/bird.92b3c4b1.svg"
+  }
+]);
