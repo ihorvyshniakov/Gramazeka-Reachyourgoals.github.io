6c4cd9a06fdeda50cad0d09ddb8f06a88a41b9e9
Vyshniakov Ihor <gramazekavip@gmail.com>
2022-07-16 18:08:40 +0300

    fixed broken-glass funtionallity

--- showcase/broken-glass-effect/index.js
+++ showcase/broken-glass-effect/index.js
@@ -1,8 +1,29 @@
-const printMousePos = e => {
-	glassBox.style.top = e.clientY + 'px';
-	glassBox.style.left = e.clientX + 'px';
-	glassBox.classList.add('cracked');
+const printMousePos = (e, isItMobile = false) => {
+	let img = document.createElement('img');
+	img.src='./files/cracked-glass.png';
+	img.alt='broken glass';
+
+	let glassItem = document.createElement('div');
+	glassItem.classList.add('glassBox');
+	glassItem.classList.add('cracked');
+
+	glassItem.appendChild(img);
+	document.body.prepend(glassItem);
+
+	if(isItMobile){
+		glassItem.style.top = e.touches[0].clientY + 'px';
+		glassItem.style.left = e.touches[0].clientX + 'px';
+	} else {
+		glassItem.style.top = e.clientY + 'px';
+		glassItem.style.left = e.clientX + 'px';
+	}
+	crackingAudio.pause();
+	crackingAudio.currentTime = 0;
 	crackingAudio.play();
 };
 
-document.addEventListener('click', printMousePos);
+if (window.innerWidth > 768) {
+	document.addEventListener('mousedown', printMousePos);
+} else {
+	document.addEventListener('touchstart', e => printMousePos(e, true));
+}
